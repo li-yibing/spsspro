c823b6326595c1b02e75c8f171f813190aa8ec5c
李宜兵 <liyibing@uict.ac.cn>
2025-04-17 20:24:05 +0800

    fix: 更新版本为v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spsspro",
-    version="0.1.0",
+    version="v0.1.1",
     description="统计分析和机器学习工具包",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
